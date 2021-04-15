3c8e849c71c2930e56142fdaa6b0ff96de6a6935
root <root@DESKTOP-IONV53U.localdomain>
2021-04-15 23:12:33 +0800

    homework complete

--- main.js
+++ main.js
@@ -5,17 +5,14 @@ const products = [
     {id: 4, title: 'Gamepad', price: 50},
 ];
 //Функция для формирования верстки каждого товара
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
+const renderProduct = (title, price) => 
+     `<div class="product-item">
                 <h3>${title}</h3>
                 <p>${price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
-};
+;
+const renderPage = list => document.querySelector('.products')
+.innerHTML = list.map(item => renderProduct(item.title, item.price)).join('');
 
 renderPage(products);
